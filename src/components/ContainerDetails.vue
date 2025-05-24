d165a30613ef69f185ab4eb2b23e4fdfae0a532e
David Mateus <mateusdavid417@gmail.com>
2025-05-24 14:24:03 -0500

    juan daniel estuvo here

--- src/components/ContainerDetails.vue
+++ src/components/ContainerDetails.vue
@@ -1,7 +1,12 @@
 <template>
-    <h1>golaaa</h1>
+    <div class="flex flex-col items-center justify-center w-full h-full border-2 border-white rounded-lg p-10">
+        <IconBrandInstagram stroke={1} />
+        <h1 class="text-white">+573225866558</h1>
+        <span class="text-white font-light">Call Us</span>
+    </div>
 </template>
 
 <script setup>
+import { IconBrandInstagram } from '@tabler/icons-vue';
 
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative h-auto w-screen overflow-hidden">
         <div class="absolute inset-0 z-0 overflow-hidden">
-            <img :src="Guacamayas" class="w-full object-cover">
+            <img :src="selvaFondo2" class="w-full h-full object-cover">
         </div>
 
         <div class="absolute inset-0 bg-black opacity-50"></div>
@@ -9,7 +9,7 @@
 
         <div class="relative z-10 flex flex-col items-center justify-center h-full p-10">
             <div class="flex flex-col lg:flex-row w-full h-auto items-center">
-                <img :src="logo" class="h-24 w-72">
+                <img :src="logo" class="hidden md:block h-24 w-72">
                 <div class="hidden md:flex w-full h-auto items-center justify-center gap-x-5">
                     <IconPointFilled class="text-white" />
                     <button class="text-white font-medium">Home</button>
@@ -26,7 +26,7 @@
                 </div>
             </div>
 
-            <div class="flex w-full h-full items-center justify-center gap-x-5">
+            <div class="flex flex-col lg:flex-row w-full h-full items-center justify-center gap-x-5 gap-y-5 mt-10">
                 <ContainerDetails />
                 <ContainerDetails />
                 <ContainerDetails />
@@ -37,7 +37,7 @@
 </template>
 
 <script setup>
-import Guacamayas from '../assets/Guacamayas.png'
+import selvaFondo2 from '../assets/selva-fondo-2.png';
 import { IconPointFilled } from '@tabler/icons-vue';
 import { IconBrandFacebook } from '@tabler/icons-vue';
 import { IconBrandInstagram } from '@tabler/icons-vue';
